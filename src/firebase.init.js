ab5aa9bd520b6d3f016b2c85f559a9a8a9ad4908
shafik720 <shafikrasel5@gmail.com>
2023-01-08 22:07:24 +0600

    trying mongodb crud

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -1,3 +1,4 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import {getAuth} from 'firebase/auth';
@@ -6,13 +7,12 @@ import {getAuth} from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "process.env.REACT_APP_apiKey",
+  apiKey: process.env.REACT_APP_apiKey,
   authDomain:process.env.REACT_APP_authDomain,
   projectId:process.env.REACT_APP_projectId,
   storageBucket:process.env.REACT_APP_storageBucket,
   messagingSenderId:process.env.REACT_APP_messagingSenderId,
-  appId:process.env.REACT_APP_appId,
-
+  appId:process.env.REACT_APP_appId
 };
 
 // Initialize Firebase
