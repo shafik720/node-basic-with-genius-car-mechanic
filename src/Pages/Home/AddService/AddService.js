bea775746ac022741208920e553b40645b921832
shafik720 <shafikrasel5@gmail.com>
2023-01-09 20:52:24 +0600

    trying mongodb crud

--- src/Pages/Home/AddService/AddService.js
+++ src/Pages/Home/AddService/AddService.js
@@ -4,24 +4,19 @@ import { useForm } from "react-hook-form";
 
 const AddService = () => {
     const { register, handleSubmit, watch, formState: { errors } } = useForm();
-    function onSubmit(){
-        
+    function onSubmit() {
+
     }
     return (
         <div>
             <h2>Add your service here</h2>
             <form onSubmit={handleSubmit(onSubmit)}>
-                {/* register your input into the hook by invoking the "register" function */}
-                <input defaultValue="test" {...register("example")} />
-
-                {/* include validation with required or other standard HTML validation rules */}
-                <input {...register("exampleRequired", { required: true })} />
-                {/* errors will return when field validation fails  */}
-                {errors.exampleRequired && <span>This field is required</span>}
-
+                <input {...register("firstName", { required: true, maxLength: 20 })} />
+                <input {...register("lastName", { pattern: /^[A-Za-z]+$/i })} />
+                <input type="number" {...register("age", { min: 18, max: 99 })} />
                 <input type="submit" />
             </form>
-        </div>
+        </div >
     );
 };
 
