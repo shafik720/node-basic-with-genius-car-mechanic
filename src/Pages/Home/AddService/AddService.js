837ad26c12cefc5e6d3f6be5abd320b4589856a5
shafik720 <shafikrasel5@gmail.com>
2023-01-09 21:18:00 +0600

    trying mongodb crud

--- src/Pages/Home/AddService/AddService.js
+++ src/Pages/Home/AddService/AddService.js
@@ -4,7 +4,23 @@ import { useForm } from "react-hook-form";
 
 const AddService = () => {
     const { register, handleSubmit } = useForm();
-    const onSubmit = data => console.log(data);
+    const onSubmit = data => {
+        console.log(data);
+
+        const url = `http://localhost:5000/addservice`;
+        fetch(url,{
+            method : 'POST',
+            headers : {
+                'content-type' : 'application/json'
+            },
+            body : JSON.stringify(data)
+        })
+        .then(res=>res.json())
+        .then(result=>{
+            console.log(result);
+        })
+
+    };
     return (
         <div>
             <h2>Add your service here</h2>
@@ -13,7 +29,7 @@ const AddService = () => {
                 <textarea className='mb-3' placeholder='Description' {...register("description",)} />
                 <input className='mb-3' placeholder='Price' type="number" {...register("price")} />
                 <input className='mb-3' placeholder='Img url'  {...register("img")} />
-                <input className='mb-3' type="submit" />
+                <input className='mb-3' type="submit"  value="Add Service"/>
             </form>
         </div >
     );
