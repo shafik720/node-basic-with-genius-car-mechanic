c2494b90776a8507a02bdf2c1517fa4dfe2cf49f
shafik720 <shafikrasel5@gmail.com>
2023-01-09 21:00:42 +0600

    trying mongodb crud

--- src/Pages/Home/AddService/AddService.js
+++ src/Pages/Home/AddService/AddService.js
@@ -3,17 +3,16 @@ import { useForm } from "react-hook-form";
 
 
 const AddService = () => {
-    const { register, handleSubmit, watch, formState: { errors } } = useForm();
-    function onSubmit() {
-
-    }
+    const { register, handleSubmit } = useForm();
+    const onSubmit = data => console.log(data);
     return (
         <div>
             <h2>Add your service here</h2>
             <form className='d-flex flex-column w-50 mx-auto' onSubmit={handleSubmit(onSubmit)}>
-                <input className='mb-3' placeholder='Your Name' {...register("name", { required: true, maxLength: 20 })} />
-                <input className='mb-3' placeholder='Description' {...register("description", )} />
+                <input className='mb-3' placeholder='Your Name' {...register("name")} />
+                <textarea className='mb-3' placeholder='Description' {...register("description",)} />
                 <input className='mb-3' placeholder='Price' type="number" {...register("price")} />
+                <input className='mb-3' placeholder='Img url'  {...register("img")} />
                 <input className='mb-3' type="submit" />
             </form>
         </div >
