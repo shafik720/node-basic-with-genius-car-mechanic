d840a36a57742523e1c0745c45f80da7820e40fc
shafik720 <shafikrasel5@gmail.com>
2023-01-10 14:07:01 +0600

    deleting data from  mongodb

--- src/Pages/Home/ManageService/ManageService.js
+++ src/Pages/Home/ManageService/ManageService.js
@@ -4,15 +4,25 @@ import useServices from '../../Hooks/useService';
 
 const ManageService = () => {
     const [services, setServices] = useServices();
-    function handleDelete(id){
-        console.log(id);
+    function handleDelete(id) {
+        const proceed = window.confirm('Are you sure you want to delete the item ? ');
+        if (proceed) {
+            fetch(`http://localhost:5000/manage/${id}`, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data);
+                    alert('Successfully deleted !');
+                })
+        }
     }
     return (
         <div>
             <h2>Manage your service here </h2>
             {
-                services.map(index=><h3 className='text-center' index={index}>{index.name}
-                    <button className='fs-6 mx-4' onClick={()=>handleDelete(index._id)}>Delete</button>
+                services.map(index => <h3 className='text-center' index={index}>{index.name}
+                    <button className='fs-6 mx-4' onClick={() => handleDelete(index._id)}>Delete</button>
                 </h3>)
             }
         </div>
