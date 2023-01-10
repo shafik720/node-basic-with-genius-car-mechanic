338f432d7ea3559d1b0658daca4b26b94039fbe9
shafik720 <shafikrasel5@gmail.com>
2023-01-10 13:48:06 +0600

    adding data to mongodb

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,16 +1,11 @@
 import React, { useEffect, useState } from 'react';
+import useServices from '../../Hooks/useService';
 import Service from '../Service/Service';
 import './Services.css';
 
 const Services = () => {
 
-    const [services, setServices] = useState([]);
-
-    useEffect( ()=>{
-        fetch('http://localhost:5000/service')
-        .then(res => res.json())
-        .then(data => setServices(data));
-    }, [])
+    const [services, setServices] = useServices();
 
     return (
         <div id="services" className='container'>
