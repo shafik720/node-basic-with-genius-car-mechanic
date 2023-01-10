7c94e198b5999095b7bb923d1e3444b22388ddfd
shafik720 <shafikrasel5@gmail.com>
2023-01-10 13:53:32 +0600

    adding data to mongodb

--- src/Pages/Home/ManageService/ManageService.js
+++ src/Pages/Home/ManageService/ManageService.js
@@ -4,12 +4,15 @@ import useServices from '../../Hooks/useService';
 
 const ManageService = () => {
     const [services, setServices] = useServices();
+    function handleDelete(id){
+        console.log(id);
+    }
     return (
         <div>
             <h2>Manage your service here </h2>
             {
                 services.map(index=><h3 className='text-center' index={index}>{index.name}
-                    <button className='fs-6 mx-4'>Delete</button>
+                    <button className='fs-6 mx-4' onClick={()=>handleDelete(index._id)}>Delete</button>
                 </h3>)
             }
         </div>
