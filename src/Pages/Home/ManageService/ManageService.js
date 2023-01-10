9910365657cdd8705152ec0d812823decd22067c
shafik720 <shafikrasel5@gmail.com>
2023-01-10 13:50:58 +0600

    adding data to mongodb

--- src/Pages/Home/ManageService/ManageService.js
+++ src/Pages/Home/ManageService/ManageService.js
@@ -1,10 +1,15 @@
 
 import React from 'react';
+import useServices from '../../Hooks/useService';
 
 const ManageService = () => {
+    const [services, setServices] = useServices();
     return (
         <div>
             <h2>Manage your service here </h2>
+            {
+                services.map(index=><h3 className='text-center' index={index}>{index.name}</h3>)
+            }
         </div>
     );
 };
