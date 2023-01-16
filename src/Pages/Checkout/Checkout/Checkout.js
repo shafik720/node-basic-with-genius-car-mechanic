793cf2b69854b4cf03dd9ad5718707ef47d9a863
shafik720 <shafikrasel5@gmail.com>
2023-01-16 20:15:34 +0600

    working on checking out page

--- src/Pages/Checkout/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout/Checkout.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import useSingleService from '../../Hooks/useSingleService';
 
 const Checkout = () => {
+    const {id} = useParams();
+    const[service] = useSingleService(id);
     return (
         <div>
-            <h2>Please Checkout your booking</h2>
+            <h2>Please Checkout your booking : {service.name} </h2>
         </div>
     );
 };
--- src/Pages/ServiceDetail/ServiceDetail.js
+++ src/Pages/ServiceDetail/ServiceDetail.js
@@ -2,21 +2,23 @@ import React from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
+import useSingleService from '../Hooks/useSingleService';
 
 const ServiceDetail = () => {
     const { serviceId } = useParams();
-    const [service, setService] = useState([]);
-    useEffect(()=>{
-        const url = `http://localhost:5000/service/${serviceId}`;
-        fetch(url)
-        .then(res=>res.json())
-        .then(data=>setService(data))
-    },[])
+    // const [service, setService] = useState([]);
+    // useEffect(()=>{
+    //     const url = `http://localhost:5000/service/${serviceId}`;
+    //     fetch(url)
+    //     .then(res=>res.json())
+    //     .then(data=>setService(data))
+    // },[])
+    const[service] = useSingleService(serviceId);
     return (
         <div>
             <h2>You are about to Book for : {service.name}</h2>
             <div className='text-center'>
-                <Link to="/checkout">
+                <Link to={`/checkout/${serviceId}`}>
                     <button className='btn btn-primary'>Proceed Checkout</button>
                 </Link>
             </div>
