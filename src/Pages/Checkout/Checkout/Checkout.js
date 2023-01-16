0be8f701e8b4109dab435a269d2121ca5cd8d3d9
shafik720 <shafikrasel5@gmail.com>
2023-01-16 20:37:24 +0600

    adding order details

--- src/Pages/Checkout/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout/Checkout.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { useParams } from 'react-router-dom';
+import auth from '../../../firebase.init';
 import useSingleService from '../../Hooks/useSingleService';
+import './Checkout.css';
 
 const Checkout = () => {
-    const {id} = useParams();
-    const[service] = useSingleService(id);
+    const { id } = useParams();
+    const [service] = useSingleService(id);
+    const [user] = useAuthState(auth);
     return (
         <div>
             <h2>Please Checkout your booking : {service.name} </h2>
+            <div className="order-form">
+                <form action="">
+                    <input value={user.displayName} type="text" name="name" id="" />
+                    <input value={user.email}  type="email" name="email" id="" />
+                    <input value={service.name} type="text" name="" id="" />
+                    <input type="text" name="address" id="" />
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
         </div>
     );
 };
