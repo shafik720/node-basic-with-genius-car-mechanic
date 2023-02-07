d800d3d57674ad9eb98fc0840f17c4b05a2f9023
shafik720 <shafikrasel5@gmail.com>
2023-02-07 21:09:36 +0600

    getting individual order information according to the individual user

--- src/Pages/Orders/Orders.js
+++ src/Pages/Orders/Orders.js
@@ -1,9 +1,25 @@
+import axios from 'axios';
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const Orders = () => {
+    const[orders, setOrders] = useState([]);
+    const[user] = useAuthState(auth);
+    useEffect(()=>{
+        const getOrder = async() =>{
+            const email = user.email;
+            const url = `http://localhost:5000/orderData`;
+            const {data} = await axios.get(url);
+            setOrders(data);
+        }
+        getOrder();
+    },[user])
     return (
         <div>
-            <h2>Order Here !</h2>
+            <h2>Order found : {orders.length} </h2>
         </div>
     );
 };
