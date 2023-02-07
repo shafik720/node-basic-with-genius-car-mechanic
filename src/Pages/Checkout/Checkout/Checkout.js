73f5da22f8c8d926ff02bfb3f79fe22987b99eb5
shafik720 <shafikrasel5@gmail.com>
2023-02-07 20:30:33 +0600

    adding data with axios

--- src/Pages/Checkout/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout/Checkout.js
@@ -9,15 +9,26 @@ const Checkout = () => {
     const { id } = useParams();
     const [service] = useSingleService(id);
     const [user] = useAuthState(auth);
+    function handleSubmit(e){
+        e.preventDefault();
+        const user = {
+            userName : e.target.name.value,
+            email : e.target.email.value,
+            serviceName : e.target.service.value,
+            address: e.target.address.value
+        }
+        
+        console.log(user);
+    }
     return (
         <div>
             <h2>Please Checkout your booking : {service.name} </h2>
             <div className="order-form">
-                <form action="">
-                    <input value={user.displayName} type="text" name="name" id="" />
-                    <input value={user.email}  type="email" name="email" id="" />
-                    <input value={service.name} type="text" name="" id="" />
-                    <input type="text" name="address" id="" />
+                <form action="" onSubmit={handleSubmit}>
+                    <input readOnly value={user.displayName} type="text" name="name" id="" />
+                    <input readOnly value={user.email}  type="email" name="email" id="" />
+                    <input readOnly value={service.name} type="text" name="service" id="" />
+                    <input type="text" name="address" placeholder='Your address' id="" />
                     <button type="submit">Submit</button>
                 </form>
             </div>
