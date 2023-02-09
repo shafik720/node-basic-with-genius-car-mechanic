42dd5ade7a99201ce3c150dc1a71fce0f0db9783
shafik720 <shafikrasel5@gmail.com>
2023-02-09 21:04:04 +0600

    sign in with jwt token

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -8,6 +8,7 @@ import SocialLogin from '../SocialLogin/SocialLogin';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import PageTitle from '../../Shared/PageTitle/PageTitle';
+import axios from 'axios';
 
 const Login = () => {
     const emailRef = useRef('');
@@ -38,12 +39,14 @@ const Login = () => {
         errorElement = <p className='text-danger'>Error: {error?.message}</p>
     }
 
-    const handleSubmit = event => {
+    const handleSubmit = async(event) => {
         event.preventDefault();
         const email = emailRef.current.value;
         const password = passwordRef.current.value;
 
-        signInWithEmailAndPassword(email, password);
+        await signInWithEmailAndPassword(email, password);
+        const{data} = await axios.post('http://localhost:5000/login', email);
+        localStorage.setItem('accessToken', data.accessToken);
     }
 
     const navigateRegister = event => {
